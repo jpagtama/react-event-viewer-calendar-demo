cc09e09dfd28841b83f1935a4f4cf3aea337eb4f
Julian Pagtama <jpagtama@gmail.com>
2022-11-30 09:43:53 -0800

    Add subheader elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,14 @@ interface CSSModuleProps {
 const Header = (props: CSSModuleProps) => {
     const styles = props.classes
     return (
-        <div className={styles.header} ><h1>react-event-viewer-calendar</h1></div>
+        <div className={styles.header} >
+            <h1>react-event-viewer-calendar</h1>
+            <div className={styles.subHeader}>
+                <span className={styles.subHeader1}>light-weight &bull;</span>
+                <span className={styles.subHeader2}>responsive &bull;</span>
+                <span className={styles.subHeader3}>customizable</span>
+            </div>
+        </div>
     )
 }
 
